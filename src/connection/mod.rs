d98bf76717df4f9e8af66f6a472e4e41643d6527
flaykDev <138144836+Flaykky@users.noreply.github.com>
2025-07-02 19:16:29 +0300

    Update mod.rs

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,1 +1,11 @@
+pub mod protocols;
 pub mod manager;
+
+pub use protocols::{
+    Protocol, Connection, ProtocolType,
+    BasicTcp, BasicUdp,
+    WireGuard, Shadowsocks, OpenVpn,
+    HttpProxy, Socks5Proxy,
+    PluginLoader, PluginProtocol, PluginConnection,
+};
+pub use manager::ConnectionManager;
