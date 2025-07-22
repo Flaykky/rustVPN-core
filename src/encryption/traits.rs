f14738ed4e14d61df85b86108cac25ddf8083be9
flaykDev <138144836+Flaykky@users.noreply.github.com>
2025-07-22 23:52:11 +0300

    Update traits.rs

--- src/encryption/traits.rs
+++ src/encryption/traits.rs
@@ -1,19 +1,61 @@
 use crate::utils::error::VpnError;
 
-/// Интерфейс для асинхронного шифрования
-pub trait AsyncCipher {
-    /// Зашифровывает данные
+/// Common interface for AEAD encryption
+pub trait AeadCipher: Send + Sync {
+    /// Encrypts data with a nonce
+    fn encrypt_with_nonce(
+        &self,
+        data: &[u8],
+        nonce: &[u8],
+    ) -> Result<Vec<u8>, VpnError>;
+
+    /// Decrypts data with a nonce
+    fn decrypt_with_nonce(
+        &self,
+        data: &[u8],
+        nonce: &[u8],
+    ) -> Result<Vec<u8>, VpnError>;
+
+    /// Returns cipher name
+    fn cipher_name(&self) -> &'static str;
+}
+
+/// Stream cipher interface for legacy ciphers
+pub trait StreamCipher: Send + Sync {
+    /// Encrypts data
     fn encrypt(&self, data: &[u8]) -> Result<Vec<u8>, VpnError>;
-    
-    /// Расшифровывает данные
+
+    /// Decrypts data
     fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>, VpnError>;
 }
 
-/// Интерфейс для AEAD-шифрования
-pub trait AeadCipher {
-    /// Зашифровывает данные с nonce
-    fn encrypt_with_nonce(&self, data: &[u8], nonce: &[u8]) -> Result<Vec<u8>, VpnError>;
-    
-    /// Расшифровывает данные с nonce
-    fn decrypt_with_nonce(&self, data: &[u8], nonce: &[u8]) -> Result<Vec<u8>, VpnError>;
+/// Interface for key derivation
+pub trait KeyDerivable: Send + Sync {
+    /// Derives a key from a password
+    fn derive_key(
+        &self,
+        password: &str,
+        salt: &[u8],
+        method: &str,
+    ) -> Result<Vec<u8>, VpnError>;
+
+    /// Generates a random key
+    fn generate_random_key(&self, method: &str) -> Result<Vec<u8>, VpnError>;
+}
+
+/// Interface for cipher factories
+pub trait CipherFactory {
+    /// Creates an AEAD cipher
+    fn create_aead(
+        &self,
+        method: &str,
+        key: &[u8],
+    ) -> Result<Box<dyn AeadCipher + Send + Sync>, VpnError>;
+
+    /// Creates a stream cipher
+    fn create_stream(
+        &self,
+        method: &str,
+        key: &[u8],
+    ) -> Result<Box<dyn StreamCipher + Send + Sync>, VpnError>;
 }
